27875d35cdab4cffa7361e8bdeb39d2432a2cf73
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-04-28 21:16:04 +0200

    Added some Greatures

--- game/map/map_builder.gd
+++ game/map/map_builder.gd
@@ -1,11 +1,15 @@
 extends Node3D
 
-var tile_scene = preload("res://tile.tscn")
+var tile_scene = preload("res://game/map/tile.tscn")
 const MAP_SIZE = 10
 @warning_ignore("integer_division")
 const OFFSET = MAP_SIZE/2
 
 var map : Dictionary[Vector3i, StaticBody3D] = {}
+
+const CREATURE_AMOUNT : int = 10
+
+var creature_scene = preload("res://game/creature/creature.tscn")
 
 func _ready() -> void:
 	var tile = tile_scene.instantiate()
@@ -18,6 +22,9 @@ func _ready() -> void:
 				$Map.add_child(tile)
 				map[Vector3i(x + OFFSET, y + OFFSET, z + OFFSET)] = tile
 	Cursor.new(map)
-
-func get_map() -> Dictionary[Vector3i, StaticBody3D]:
-	return map
+	var creature_tiles = map.keys()
+	creature_tiles.shuffle()
+	creature_tiles = creature_tiles.slice(0, CREATURE_AMOUNT)
+	for creature_tile in creature_tiles:
+		var creature = creature_scene.instantiate()
+		map[creature_tile].add_child(creature)
